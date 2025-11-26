6565a8ded396a01b066c8aa501b41a9b3e2eb91f
Jersonmade <dima.sergeev.0404@inbox.ru>
2025-11-26 21:13:13 +0400

    commit fourteenth task 2

--- 14/main.go
+++ 14/main.go
@@ -15,21 +15,8 @@ func getType(param any) {
 		fmt.Printf("Булевый тип %T", v)
 	default:
 		t := reflect.TypeOf(param)
-
-		switch t.Kind() {
-		case reflect.Chan:
-			elemType := t.Elem()
-			fmt.Printf("Канальный тип: chan %s", elemType)
-		case reflect.Slice:
-			elemType := t.Elem()
-			fmt.Printf("Слайс: []%s", elemType)
-		case reflect.Map:
-			keyType := t.Key()
-			elemType := t.Elem()
-			fmt.Printf("Хеш-таблица: map[%s]%s", keyType, elemType)
-		default:
-			fmt.Printf("Неизвестный тип %s", t)
-		}
+		elemType := t.Elem()
+		fmt.Printf("Канальный тип: chan %s", elemType)
 	}
 
 	fmt.Println()
@@ -39,14 +26,10 @@ func main() {
 	a := 10
 	str := "hkcbakcb"
 	flag := true
-	ch := make(chan bool)
-	var slice []int
-	m := make(map[string]int)
+	ch := make(chan int)
 
 	getType(a)
 	getType(str)
 	getType(flag)
 	getType(ch)
-	getType(slice)
-	getType(m)
 }
