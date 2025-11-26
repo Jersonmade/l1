30b170347cccbd8017a03cbfaa871bceb0881e44
Jersonmade <dima.sergeev.0404@inbox.ru>
2025-11-26 21:07:30 +0400

    fourteenth task

---
+++ 14/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func getType(param any) {
+	switch v := param.(type) {
+	case int:
+		fmt.Printf("Целочисленный тип %T", v)
+	case string:
+		fmt.Printf("Строковый тип %T", v)
+	case bool:
+		fmt.Printf("Булевый тип %T", v)
+	default:
+		t := reflect.TypeOf(param)
+
+		switch t.Kind() {
+		case reflect.Chan:
+			elemType := t.Elem()
+			fmt.Printf("Канальный тип: chan %s", elemType)
+		case reflect.Slice:
+			elemType := t.Elem()
+			fmt.Printf("Слайс: []%s", elemType)
+		case reflect.Map:
+			elemType := t.Elem()
+			keyType := t.Key()
+			fmt.Printf("Хеш-таблица: map[%s]%s", keyType, elemType)
+		default:
+			fmt.Printf("Неизвестный тип %s", t)
+		}
+		
+	}
+
+	fmt.Println()
+}
+
+func main() {
+	a := 10
+	str := "hkcbakcb"
+	flag := true
+	ch := make(chan bool)
+	var slice []int
+	m := make(map[string]int)
+
+	getType(a)
+	getType(str)
+	getType(flag)
+	getType(ch)
+	getType(slice)
+	getType(m)
+}
