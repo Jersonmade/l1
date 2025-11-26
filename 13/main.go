83509e5b5287653ddea4ef46ecb06e29fc01b764
Jersonmade <dima.sergeev.0404@inbox.ru>
2025-11-26 19:03:54 +0400

    thirteenth task

---
+++ 13/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 121
+	b := 365
+	
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+
+	a = a + b
+	b = a - b
+	a = a - b
+	
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+}
